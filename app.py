06362ea103027d7bf013b8d07c8d8cb41d83f338
Nipun Dhiman <nipundhiman03@gmail.com>
2024-02-06 17:52:55 +0530

    2nd_commit

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
 import streamlit as st
 import openai
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    
+    return text
+    
+def get_text_chunks(raw_text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size = 1000,
+        chunk_overlap=200,
+        length_function = len
+    )
+    
+    chunks = text_splitter.split_text(raw_text)
+    return chunks
+    
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with Multiple PDFs", page_icon=":books:")
@@ -11,9 +34,18 @@ def main():
     
     with st.sidebar:
         st.subheader("Your Documents")
-        st.file_uploader("Upload your PDFs here and click on Process'")
-        st.button("Process")
-        
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                #Get the pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
+                
+                #Get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+                st.write(text_chunks)
+                #create vector store
+
         
 if __name__ == '__main__':
     main()
