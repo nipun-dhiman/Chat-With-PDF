247cd05de19a95651297d58d1fdda5a34813e46b
Nipun Dhiman <nipundhiman03@gmail.com>
2024-02-07 00:41:58 +0530

    adding_chatcompletion_chain

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
-from langchain.vectorstores import faiss
-
+from langchain_community.vectorstores import FAISS
+from langchain.chat_models import  ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+# from htmlTemplates import css, bot_template, user_template
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -31,14 +34,26 @@ def get_text_chunks(raw_text):
     
 def get_vectorstore(text_chunks):
     embeddings = OpenAIEmbeddings()
-    vectorstore = faiss.from_texts(texts = text_chunks, embedding=embeddings)
+    # embeddings =  HuggingFaceInstructEmbeddings(model_name ="hkunlp/instructor-xl" )
+    vectorstore = FAISS.from_texts(texts = text_chunks, embedding=embeddings)
     return vectorstore
     
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key="Chat_history", return_messages = True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        retriever=vectorstore.as_retriever(),
+        memory = memory
+    )
+    return conversation_chain
     
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with Multiple PDFs", page_icon=":books:")
 
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
     st.header("Chat with  multiple PDFs :books:")
     st.text_input("Ask a question about your documents: ")
     
@@ -58,6 +73,12 @@ def main():
                 #create vector store
                 vectorstore = get_vectorstore(text_chunks)
                 
-        
+                #create converstion chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+    
+    
+                 
+                
+                
 if __name__ == '__main__':
     main()
