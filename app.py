b740f214f3a4abe3250cfdfc90463fbff98f2468
Nipun Dhiman <nipundhiman03@gmail.com>
2024-02-07 01:45:34 +0530

    Added_template and implemented chat

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain_community.vectorstores import FAISS
 from langchain.chat_models import  ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-# from htmlTemplates import css, bot_template, user_template
+from htmlTemplates import css, bot_template, user_template
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -40,22 +40,44 @@ def get_vectorstore(text_chunks):
     
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI()
-    memory = ConversationBufferMemory(memory_key="Chat_history", return_messages = True)
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages = True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm = llm,
         retriever=vectorstore.as_retriever(),
         memory = memory
     )
     return conversation_chain
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+    
+    for i, message in enumerate(st.session_state.chat_history):
+        if i%2 == 0:
+            st.write(user_template.replace("{{MSG}}",message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}",message.content), unsafe_allow_html=True)
     
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with Multiple PDFs", page_icon=":books:")
 
+    st.write(css, unsafe_allow_html=True)
+    
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
+        
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
     st.header("Chat with  multiple PDFs :books:")
-    st.text_input("Ask a question about your documents: ")
+    user_question = st.text_input("Ask a question about your documents: ")
+    
+    st.write(user_template.replace("{{MSG}}","Hello Robot"), unsafe_allow_html=True)
+    st.write(bot_template.replace("{{MSG}}","Hello Animal"), unsafe_allow_html=True)
+    
+    
+    if user_question:
+        handle_userinput(user_question)
     
     with st.sidebar:
         st.subheader("Your Documents")
@@ -76,9 +98,7 @@ def main():
                 #create converstion chain
                 st.session_state.conversation = get_conversation_chain(vectorstore)
     
-    
-                 
-                
+       
                 
 if __name__ == '__main__':
     main()
